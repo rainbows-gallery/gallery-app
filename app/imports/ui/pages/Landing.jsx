ad8b8a93d1133ed6a157d1ef849cf5ee24f78638
SienneR <rodwells@hawaii.edu>
2023-11-08 19:05:39 -1000

    Initialization
    NavBar name
    get started button
    import logo image
    uploaded background image -- Could not get it to show up
    change navbar + footer to silver
    Change tab title
    Tried to get button color to be green - failed.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
-      </Col>
-
+      <h1>Content and Design can go here</h1>
     </Row>
   </Container>
 );
