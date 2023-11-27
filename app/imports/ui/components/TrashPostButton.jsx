eb29a6c3d0c044c7f73af69f1791cb5055c293c0
Eric Zhou <ericzhougit@gmail.com>
2023-11-26 15:21:36 -1000

    Issue #39: Added more properties to TrashPostButton
    - Button removes comments associated with the deleted post
    - Added redirectTo property to navigate to given directory after deleting post

--- app/imports/ui/components/TrashPostButton.jsx
+++ app/imports/ui/components/TrashPostButton.jsx
@@ -4,22 +4,33 @@ import { Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import { Posts } from '../../api/Posts/Posts';
+import { Comments } from '../../api/comment/Comments';
 
-const TrashPostButton = ({ postId }) => {
+const TrashPostButton = ({ postId, comments, redirectTo }) => {
   const navigate = useNavigate();
 
-  const removePost = function (id) {
+  // Remove post and its associated comments from collection and navigate to another page
+  const removePost = function (id, commentArr) {
+    commentArr.map((comment) => Comments.collection.remove(comment._id));
     Posts.collection.remove(id);
-    navigate('/');
+    navigate(redirectTo);
   };
 
   return (
-    <Button variant="link" className="text-danger ps-0" onClick={() => removePost(postId)}><Trash3Fill size={30} /></Button>
+    <Button variant="link" className="text-danger ps-0" onClick={() => removePost(postId, comments, redirectTo)}><Trash3Fill size={30} /></Button>
   );
 };
 
 TrashPostButton.propTypes = {
   postId: PropTypes.string.isRequired,
+  comments: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string,
+    comment: PropTypes.string,
+    postId: PropTypes.string,
+    createdAt: PropTypes.objectOf(Date),
+    owner: PropTypes.string,
+  })).isRequired,
+  redirectTo: PropTypes.string.isRequired,
 };
 
 export default TrashPostButton;
