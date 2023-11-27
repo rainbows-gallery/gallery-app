a180bf83b1900632b40fc1a2b3355353054f0c20
SienneR <rodwells@hawaii.edu>
2023-11-26 15:57:41 -1000

    Pretty bad start

--- app/imports/ui/pages/EditProfile.jsx
+++ app/imports/ui/pages/EditProfile.jsx
@@ -3,39 +3,36 @@
 import React from 'react';
 import swal from 'sweetalert';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import { useParams } from 'react-router';
 import LoadingSpinner from '../components/LoadingSpinner';
-import { Contacts } from '../../api/contact/Contacts';
 
-const bridge = new SimpleSchema2Bridge(Contacts.schema);
+/* Renders the EditProfile page for editing a single document. */
+const EditProfile = () => {
 
-/* Renders the EditContact page for editing a single document. */
-const EditContact = () => {
-  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
-  const { _id } = useParams();
-  // console.log('EditContact', _id);
-  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { doc, ready } = useTracker(() => {
+  const { user, ready } = useTracker(() => {
     // Get access to Contact documents.
-    const subscription = Meteor.subscribe(Contacts.userPublicationName);
+    const subscription = Meteor.subscribe('userList');
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the document
-    const document = Contacts.collection.findOne(_id);
+    const userFetch = Meteor.user();
     return {
-      doc: document,
+      user: userFetch,
       ready: rdy,
     };
-  }, [_id]);
-  // console.log('EditContact', doc, ready);
-  // On successful submit, insert the data.
+  }, []);
+
   const submit = (data) => {
-    const { firstName, lastName, address, image, description } = data;
-    Contacts.collection.update(_id, { $set: { firstName, lastName, address, image, description } }, (error) => (error ?
+    const { image } = data;
+    Meteor.users.update(user, {
+      $set: {
+        profile: {
+          image,
+        },
+      },
+    }, (error) => (error ?
       swal('Error', error.message, 'error') :
       swal('Success', 'Item updated successfully', 'success')));
   };
@@ -45,26 +42,14 @@ const EditContact = () => {
       <Row className="justify-content-center">
         <Col xs={10}>
           <Col className="text-center"><h2>Edit Contact</h2></Col>
-          <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
+          <AutoForm onSubmit={data => submit(data)}>
             <Card>
               <Card.Body>
                 <Row>
                   <Col>
-                    <TextField name="firstName" />
-                  </Col>
-                  <Col>
-                    <TextField name="lastName" />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <TextField name="address" />
-                  </Col>
-                  <Col>
                     <TextField name="image" />
                   </Col>
                 </Row>
-                <LongTextField name="description" />
                 <SubmitField value="Submit" />
                 <ErrorsField />
               </Card.Body>
@@ -76,4 +61,4 @@ const EditContact = () => {
   ) : <LoadingSpinner />;
 };
 
-export default EditContact;
+export default EditProfile;
